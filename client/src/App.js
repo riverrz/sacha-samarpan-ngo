46cefc48c48ddd18e9212791537f9f7ed7f08760
shivam123425 <shivam123425@gmail.com>
2018-06-14 23:28:47 +0530

    Add Backside component at top

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,18 @@ import Navigation from './components/Navigation/Navigation';
 import Maintext from './components/Maintext/Maintext';
 import Carousel from './components/Carousel/Carousel';
 import Gallery from './components/Gallery/Gallery';
+import Footer from './components/Footer/Footer';
+import Backside from './components/Backside/Backside';
 class App extends Component {
   render() {
     return (
       <div className={classes.App}>
+        <Backside />
         <Navigation />
         <Carousel />
         <Maintext />
         <Gallery />
+        <Footer />
       </div>
     );
   }
