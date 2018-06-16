adda6affd372176610fa961bc44decfed0e1941e
shivam123425 <shivam123425@gmail.com>
2018-06-16 22:56:22 +0530

    Changed Gallery to stateful component with images URL in state

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Gallery.css";
 import GalleryItem from "./GalleryItem/GalleryItem";
-const gallery = props => {
-  return (
-    <div className={classes.galleryContainer}>
-      <h1 className={classes.gallery__headingPrimary}>
-        Our Precious <span>Moments</span>
-      </h1>
-      <div className={classes.gallery}>
-        <GalleryItem imageName="2.JPG" text="test" />
-        <GalleryItem imageName="2.JPG" text="test" />
-        <GalleryItem imageName="2.JPG" text="test" />
-        <GalleryItem imageName="2.JPG" text="test" />
-        <GalleryItem imageName="2.JPG" text="test" />
-        <GalleryItem imageName="2.JPG" text="test" />
-        <GalleryItem imageName="2.JPG" text="test" />
-        <GalleryItem imageName="2.JPG" text="test" />
-      </div>
-    </div>
-  );
-};
 
-export default gallery;
+class Gallery extends Component {
+  state={
+    images: [
+      {
+        name:"2.JPG",
+        text: "test1"
+      },
+      {
+        name:"2.JPG",
+        text: "test2"
+      },
+      {
+        name:"2.JPG",
+        text: "test"
+      },
+      {
+        name:"2.JPG",
+        text: "test"
+      },
+      {
+        name:"2.JPG",
+        text: "test"
+      },
+      {
+        name:"2.JPG",
+        text: "test"
+      },
+      {
+        name:"2.JPG",
+        text: "test"
+      },
+      {
+        name:"2.JPG",
+        text: "test"
+      },
+    ]
+  }
+  render() {
+    const allGalleryItems = this.state.images.map((data)=> {
+      return <GalleryItem imageName={data.name} text={data.text} />
+    })
+    return (
+      <div className={classes.galleryContainer}>
+        <h1 className={classes.gallery__headingPrimary}>
+          Our Precious <span>Moments</span>
+        </h1>
+        <div className={classes.gallery}>
+          {allGalleryItems}
+        </div>
+      </div>
+    );
+  }
+}
+export default Gallery;
