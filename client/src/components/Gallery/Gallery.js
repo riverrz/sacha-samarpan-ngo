87750df410594fe830b43dff1c4e57dfbd610e1b
shivam123425 <shivam123425@gmail.com>
2018-06-18 23:32:36 +0530

    Add View All button in Gallery, adjusted margin of Carousel

--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -1,56 +1,60 @@
 import React, { Component } from "react";
 import classes from "./Gallery.css";
 import GalleryItem from "./GalleryItem/GalleryItem";
+import { Link } from "react-router-dom";
 
 class Gallery extends Component {
-  state={
+  state = {
     images: [
       {
-        name:"2.JPG",
+        name: "2.JPG",
         text: "test1"
       },
       {
-        name:"2.JPG",
+        name: "2.JPG",
         text: "test2"
       },
       {
-        name:"2.JPG",
+        name: "2.JPG",
         text: "test"
       },
       {
-        name:"2.JPG",
+        name: "2.JPG",
         text: "test"
       },
       {
-        name:"2.JPG",
+        name: "2.JPG",
         text: "test"
       },
       {
-        name:"2.JPG",
+        name: "2.JPG",
         text: "test"
       },
       {
-        name:"2.JPG",
+        name: "2.JPG",
         text: "test"
       },
       {
-        name:"2.JPG",
+        name: "2.JPG",
         text: "test"
-      },
+      }
     ]
-  }
+  };
   render() {
-    const allGalleryItems = this.state.images.map((data,i)=> {
-      return <GalleryItem imageName={data.name} text={data.text} key={i}/>
-    })
+    const allGalleryItems = this.state.images.map((data, i) => {
+      return <GalleryItem imageName={data.name} text={data.text} key={i} />;
+    });
     return (
       <div className={classes.galleryContainer}>
         <h1 className={classes.gallery__headingPrimary}>
           Our Precious <span>Moments</span>
         </h1>
-        <div className={classes.gallery}>
-          {allGalleryItems}
-        </div>
+        <div className={classes.gallery}>{allGalleryItems}</div>
+        <button className={classes.galleryLink}>
+          <Link to="/gallery" exact>
+            View All Photos <span className={classes.arrow}>→</span>
+          </Link>
+        </button>
       </div>
     );
   }
