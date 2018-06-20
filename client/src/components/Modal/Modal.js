f600957e62b163a0c4733a043cb8cf7541ba3003
shivam123425 <shivam123425@gmail.com>
2018-06-20 23:21:27 +0530

    Add fa-times in Modal in exitButton

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -4,9 +4,11 @@ const modal=props=> {
     var indexOfLink=props.image.indexOf('/images');
     const relativeLink = props.image.slice(indexOfLink);
     return <div className={classes.modalContainer}>
-        <button className={classes.exitButton} onClick={props.exitClicked}>X</button>
-        <img src={relativeLink} alt="Invoked"/>
-    </div>;
+        <button className={classes.exitButton} onClick={props.exitClicked}>
+          <i class="fas fa-times" />
+        </button>
+        <img src={relativeLink} alt="Invoked" />
+      </div>;
 };
 
 export default modal;
