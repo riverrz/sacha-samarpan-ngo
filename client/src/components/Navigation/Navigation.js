d5f89e0ca192715cf8ec2ef12c971fbf6eb39000
shivam123425 <shivam123425@gmail.com>
2018-06-23 22:42:38 +0530

    Fixed navigation instead of nav, increased z-index of Backdrop

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -33,17 +33,17 @@ class Navigation extends Component {
     let styleForFixedNav = {
       position: "absolute",
       left: "50%",
-      top: "0",
+      top: "4rem",
       width: "80%",
-      transform: "translateX(-50%)"
+      
     };
     if (this.state.scrollingLock) {
-      styleForFixedNav = { position: "fixed", top: "0", width: "100%" };
+      styleForFixedNav = { position: "fixed", top: "0", width: "100%", transform: "translateX(0)" };
     }
     console.log(styleForFixedNav);
     return (
       <Aux>
-        <div className={classes.nav} style={styleForFixedNav}>
+        <div className={classes.nav}>
           <div className={classes.nav__contact}>
             <div className={classes.nav__contact__number}>
               <i class="fas fa-phone" /> 9999999999
@@ -60,89 +60,89 @@ class Navigation extends Component {
               </a>
             </div>
           </div>
-          <nav className={classes.navigation}>
-            <div
-              className={classNames({
-                [classes.navigation__item]: true,
-                [classes.logoBox]: true
-              })}
-            >
-              <img className={classes.logo} src={Logo} alt="Logo" />
-            </div>
-            <div className={classes.navigation__item}>
-              <Link to="/" exact>
-                Home
-              </Link>
-            </div>
-            <div className={classes.navigation__item}>
-              <NavLink to="/about" activeClassName={classes.active}>
-                About
-              </NavLink>
-              <Dropdown>
-                <li>
-                  <Link to="/about" exact>
-                    <i class="fas fa-angle-right" /> About Us
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/founders" exact>
-                    <i class="fas fa-angle-right" /> Founders
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/team" exact>
-                    <i class="fas fa-angle-right" /> Our Team
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/issues" exact>
-                    <i class="fas fa-angle-right" /> Issues & Views
-                  </Link>
-                </li>
-              </Dropdown>
-            </div>
-
-            <div className={classes.navigation__item}>
-              <Link to="/full-gallery" exact>
-                Gallery
-              </Link>
-            </div>
-            <div className={classes.navigation__item}>
-              <Link to="/internship" exact>
-                Internship
-              </Link>
-            </div>
-            <div className={classes.navigation__item}>
-              <a href="/">Programs</a>
-              <Dropdown>
-                <li>
-                  <a href="/">
-                    <i class="fas fa-angle-right" /> Program 1
-                  </a>
-                </li>
-                <li>
-                  <a href="/">
-                    <i class="fas fa-angle-right" /> Program 2
-                  </a>
-                </li>
-              </Dropdown>
-            </div>
-            <div className={classes.navigation__item}>
-              <a href="/">Registration</a>
-            </div>
-            <div className={classes.navigation__item}>
-              <Link to="/important-links" exact>
-                Important Links
-              </Link>
-            </div>
-            <div className={classes.navigation__item}>
-              <Link to="/donate" exact>
-                Donate
-              </Link>
-            </div>
-          </nav>
         </div>
+        <nav className={classes.navigation} style={styleForFixedNav}>
+          <div
+            className={classNames({
+              [classes.navigation__item]: true,
+              [classes.logoBox]: true
+            })}
+          >
+            <img className={classes.logo} src={Logo} alt="Logo" />
+          </div>
+          <div className={classes.navigation__item}>
+            <Link to="/" exact>
+              Home
+            </Link>
+          </div>
+          <div className={classes.navigation__item}>
+            <NavLink to="/about" activeClassName={classes.active}>
+              About
+            </NavLink>
+            <Dropdown>
+              <li>
+                <Link to="/about" exact>
+                  <i class="fas fa-angle-right" /> About Us
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/founders" exact>
+                  <i class="fas fa-angle-right" /> Founders
+                </Link>
+              </li>
+              <li>
+                <Link to="/team" exact>
+                  <i class="fas fa-angle-right" /> Our Team
+                </Link>
+              </li>
+              <li>
+                <Link to="/issues" exact>
+                  <i class="fas fa-angle-right" /> Issues & Views
+                </Link>
+              </li>
+            </Dropdown>
+          </div>
+
+          <div className={classes.navigation__item}>
+            <Link to="/full-gallery" exact>
+              Gallery
+            </Link>
+          </div>
+          <div className={classes.navigation__item}>
+            <Link to="/internship" exact>
+              Internship
+            </Link>
+          </div>
+          <div className={classes.navigation__item}>
+            <a href="/">Programs</a>
+            <Dropdown>
+              <li>
+                <a href="/">
+                  <i class="fas fa-angle-right" /> Program 1
+                </a>
+              </li>
+              <li>
+                <a href="/">
+                  <i class="fas fa-angle-right" /> Program 2
+                </a>
+              </li>
+            </Dropdown>
+          </div>
+          <div className={classes.navigation__item}>
+            <a href="/">Registration</a>
+          </div>
+          <div className={classes.navigation__item}>
+            <Link to="/important-links" exact>
+              Important Links
+            </Link>
+          </div>
+          <div className={classes.navigation__item}>
+            <Link to="/donate" exact>
+              Donate
+            </Link>
+          </div>
+        </nav>
       </Aux>
     );
   }
