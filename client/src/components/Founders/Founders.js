bee1025d06460ac62267cac2e03830d21929018e
shivam123425 <shivam123425@gmail.com>
2018-06-23 15:27:31 +0530

    Complete Founders, fixed extra top margin on components, added more pictures

--- client/src/components/Founders/Founders.js
+++ client/src/components/Founders/Founders.js
@@ -14,6 +14,16 @@ const founders = props => {
             className={classes.founderPhoto}
           />
         </figure>
+        <div className={classes.foundersInfo}>
+          <h2 className={classes.designation}>President</h2>
+          Nauratna Gupta is the President of Saccha Samarpan Society. He has 15
+          years of experience in Politics, Social Work and Business. He is
+          dedicated towards providing quality education to all poor child and
+          motivate them for their future desires. He worked with NGOs, Nishant
+          Nirala Educational & Welfare Society as a social worker to promote
+          education in rural areas. He is the Director of Shri Kalka
+          Construction Company.
+        </div>
       </div>
       <div className={classes.founder}>
         <figure className={classes.figure}>
@@ -23,6 +33,16 @@ const founders = props => {
             className={classes.founderPhoto}
           />
         </figure>
+        <div className={classes.foundersInfo}>
+          <h2 className={classes.designation}>General Secretary</h2>
+          Reena Sharma is the General Secretory of Saccha Samarpan Society. She
+          has taken an initiative to educate the child and supporting the women
+          equality. She is the Director of Mahila Training Institue (MTI).She
+          has worked with many NGOs for the bettlement of the society regarding
+          Women Empowerment and Child Education.She has 20 year of experience in
+          Social Workand Teaching different Vocational courses in different NGOs
+          and Institute.
+        </div>
       </div>
     </div>
   );
