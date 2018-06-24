ec33054f76aae32bc6ec072eb2630020d8ee76f3
shivam123425 <shivam123425@gmail.com>
2018-06-24 18:27:16 +0530

    fixed port to PORT as env variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
