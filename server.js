a1b84017e0bec98b3c299e5685d8dae86cf686c0
shivam123425 <shivam123425@gmail.com>
2018-06-24 14:36:09 +0530

    Add proxy in package,json(react), add engine and postBuild script in package.json(server)

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
 const port = process.env.port || 5000;
 
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => {
   console.log("Server has started");
