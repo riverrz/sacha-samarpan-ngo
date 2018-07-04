a09277e06d30d24a88b630ddf2e5e1d1c669e1be
shivam123425 <shivam123425@gmail.com>
2018-07-04 21:06:52 +0530

    Add mlab's mongodb replacing local mongodb

--- server.js
+++ server.js
@@ -3,29 +3,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const Member = require("./Models/Member");
+const { mongoURI } = require("./config/keys");
+
 const app = express();
 
 const port = process.env.PORT || 5000;
-mongoose.connect("mongodb://localhost/sacha_samarpan");
 
+mongoose.connect(mongoURI);
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/registration", async (req,res)=> {
+app.post("/registration", async (req, res) => {
   const possibleNewMember = req.body.member;
   const attrToFind = {
     name: possibleNewMember.name,
     phone: possibleNewMember.phone
-  }
+  };
   const foundMember = await Member.findOne(attrToFind);
   if (foundMember) {
     return res.send("You have already registered");
   }
   const newMember = await Member.create(possibleNewMember);
-  
-  res.send("Thank You for becoming a member of our NGO. We wish a great journey helping needy people. Thank you again")
-})
+
+  res.send(
+    "Thank You for becoming a member of our NGO. We wish a great journey helping needy people. Thank you again"
+  );
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
