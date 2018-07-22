899e2240437021d9b1ada11e69e2347f73334530
shivam123425 <shivam123425@gmail.com>
2018-07-22 12:51:34 +0530

    Remove a tag styling, and localised to the Component, add social links

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -32,150 +32,120 @@ class Navigation extends Component {
     if (this.state.scrollingLock) {
       navigationClass = 'navigation--fixed';
     }
-    return (
-      <Fragment>
-        <div className='nav'>
-          <div className='nav__contact'>
-            <div className='nav__contact__number'>
+    return <Fragment>
+        <div className="nav">
+          <div className="nav__contact">
+            <div className="nav__contact__number">
               <i className="fas fa-phone" /> +91 9211458806
             </div>
-            <div className='nav__contact__email'>
+            <div className="nav__contact__email">
               <i className="fas fa-envelope" /> info@sacchasamarpan.com
             </div>
-            <div className='nav__contact__social'>
-              <a href="/">
-                <i className="fab fa-facebook" />
+            <div className="nav__contact__social">
+              <a href="https://www.facebook.com/SacchaSamarpanNGO" target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-facebook-f" />
               </a>
-              <a href="/">
-                <i className="fab fa-google-plus" />
+              <a href="https://www.twitter.com/SacchaSamarpan1"  target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-twitter" />
               </a>
-              <a href="/">
+              <a href="https://www.instagram.com/sacchasamarpan_ngo"  target="_blank" rel="noopener noreferrer">
+                <i className="fab fa-instagram" />
+              </a>
+              <a href="/"  target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-whatsapp" />
               </a>
             </div>
           </div>
         </div>
         <nav className={`navigation ${navigationClass}`}>
-          <div className='navigation__item navigation__logoBox'>
-            <img className='navigation__logo' src={Logo} alt="Logo" />
+          <div className="navigation__item navigation__logoBox">
+            <img className="navigation__logo" src={Logo} alt="Logo" />
           </div>
-          <div className='navigation__item'>
-            <NavLink to="/" exact activeClassName='navigation__active'>
+          <div className="navigation__item">
+            <NavLink to="/" exact activeClassName="navigation__active">
               Home
             </NavLink>
           </div>
-          <div className='navigation__item'>
-            <NavLink to="/about" activeClassName='navigation__active'>
+          <div className="navigation__item">
+            <NavLink to="/about" activeClassName="navigation__active">
               About
             </NavLink>
             <Dropdown>
               <li>
-                <NavLink
-                  to="/about"
-                  exact
-                  activeClassName='navigation__activeDropDown'
-                >
+                <NavLink to="/about" exact activeClassName="navigation__activeDropDown">
                   <i className="fas fa-angle-right" /> Overview
                 </NavLink>
               </li>
 
               <li>
-                <NavLink
-                  to="/about/founders"
-                  exact
-                  activeClassName='navigation__activeDropDown'
-                >
+                <NavLink to="/about/founders" exact activeClassName="navigation__activeDropDown">
                   <i className="fas fa-angle-right" /> Founders
                 </NavLink>
               </li>
               <li>
-                <NavLink
-                  to="/about/team"
-                  exact
-                  activeClassName='navigation__activeDropDown'
-                >
+                <NavLink to="/about/team" exact activeClassName="navigation__activeDropDown">
                   <i className="fas fa-angle-right" /> Our Team
                 </NavLink>
               </li>
               <li>
-                <NavLink
-                  to="/about/issues"
-                  exact
-                  activeClassName='navigation__activeDropDown'
-                >
+                <NavLink to="/about/issues" exact activeClassName="navigation__activeDropDown">
                   <i className="fas fa-angle-right" /> Issues & Views
                 </NavLink>
               </li>
             </Dropdown>
           </div>
 
-          <div className='navigation__item'>
-            <NavLink to="/full-gallery" exact activeClassName='navigation__active'>
+          <div className="navigation__item">
+            <NavLink to="/full-gallery" exact activeClassName="navigation__active">
               Gallery
             </NavLink>
           </div>
-          <div className='navigation__item'>
-            <NavLink to="/internship" exact activeClassName='navigation__active'>
+          <div className="navigation__item">
+            <NavLink to="/internship" exact activeClassName="navigation__active">
               Internship
             </NavLink>
           </div>
 
-          <div className='navigation__item'>
-            <NavLink to="/events" activeClassName='navigation__active'>
+          <div className="navigation__item">
+            <NavLink to="/events" activeClassName="navigation__active">
               Events
             </NavLink>
             <Dropdown>
               <li>
-                <NavLink
-                  to="/events"
-                  exact
-                  activeClassName='navigation__activeDropDown'>
+                <NavLink to="/events" exact activeClassName="navigation__activeDropDown">
                   <i className="fas fa-angle-right" /> Overview
                 </NavLink>
               </li>
               <li>
-                <NavLink
-                  to="/events/archive"
-                  exact
-                  activeClassName='navigation__activeDropDown'>
+                <NavLink to="/events/archive" exact activeClassName="navigation__activeDropDown">
                   <i className="fas fa-angle-right" /> Events Archive
                 </NavLink>
               </li>
               <li>
-                <NavLink
-                  to="/events/upcoming"
-                  exact
-                  activeClassName='navigation__activeDropDown'>
+                <NavLink to="/events/upcoming" exact activeClassName="navigation__activeDropDown">
                   <i className="fas fa-angle-right" /> Upcoming Events
                 </NavLink>
               </li>
             </Dropdown>
           </div>
 
-          <div className='navigation__item'>
-            <NavLink to="/registration" exact activeClassName='navigation__active'>
+          <div className="navigation__item">
+            <NavLink to="/registration" exact activeClassName="navigation__active">
               Registration
             </NavLink>
           </div>
-          <div className='navigation__item'>
-            <NavLink
-              to="/important-links"
-              exact
-              activeClassName='navigation__active'
-            >
+          <div className="navigation__item">
+            <NavLink to="/important-links" exact activeClassName="navigation__active">
               Important Links
             </NavLink>
           </div>
-          <div className='navigation__item navigation__donate'>
+          <div className="navigation__item navigation__donate">
             <Link to="/donate" exact>
-              <strong className='navigation__donate__strong'>
-                Donate
-              </strong>
+              <strong className="navigation__donate__strong">Donate</strong>
             </Link>
           </div>
         </nav>
-      </Fragment>
-    );
+      </Fragment>;
   }
 }
 
