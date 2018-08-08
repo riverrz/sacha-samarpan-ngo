8cd2800ade7410f72ed13e4aecdba370e00a6c7d
shivam123425 <shivam123425@gmail.com>
2018-08-08 23:29:08 +0530

    Add email form in Contact without backend

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -26,7 +26,47 @@ const contact = props => {
       </aside>
       <main className="contact__main">
         <h1 className="contact__main__heading--primary">Contact Us</h1>
-
+        <div className="contact__main__container">
+          <form action="/" className="contact__emailForm" autoComplete="off" spellCheck={ false }>
+            <div className="contact__form__inputContainer">
+              <input
+                type="text"
+                className="contact__input"
+                placeholder="First Name"
+                name="message[fName]"
+              />
+              <input
+                type="text"
+                className="contact__input"
+                placeholder="Last Name"
+                name="message[lName]"
+              />
+              <input
+                type="email"
+                className="contact__input"
+                placeholder="Email Address"
+                name="message[email]"
+              />
+              <input
+                type="text"
+                className="contact__input"
+                placeholder="Phone No."
+                name="message[phoneNo]"
+              />
+              <textarea
+                name="message[message]"
+                className="contact__textArea"
+                cols="30"
+                rows="10"
+                placeholder="Your Message"
+              />
+            </div>
+            <button className="contact__form__submitBtn">Submit</button>
+          </form>
+          <div className="contact__main__altContainer">
+            <h1>Alternatives</h1>
+          </div>
+        </div>
       </main>
     </WhiteContainer>
   );
