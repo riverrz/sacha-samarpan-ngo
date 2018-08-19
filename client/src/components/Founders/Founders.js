c064379f31ebc0d780b0328f50f85d167d2ab5e8
shivam123425 <shivam123425@gmail.com>
2018-08-19 16:16:32 +0530

    Keep font-size and line-height consistent

--- client/src/components/Founders/Founders.js
+++ client/src/components/Founders/Founders.js
@@ -60,13 +60,15 @@ const founders = props => {
             {" "}
             Member of ZRUCC in Railway Ministry (Government of India).
           </strong>{" "}
-          He is well qualified and holds his Masters degree in Master in Social
-          Work (MSW). He has more than 18 years of experience in Social Work,
-          Politics and Business. He is dedicated towards providing quality
-          education to all poor children and motivates them for their future
-          desires. He has worked with many NGO’s for the enhancement of the
-          society regarding Women Empowerment and Child Education. He is also
-          the Director of “Shri Kalka Construction Company”.
+          <p>
+            He is well qualified and holds his Masters degree in Master in
+            Social Work (MSW). He has more than 18 years of experience in Social
+            Work, Politics and Business. He is dedicated towards providing
+            quality education to all poor children and motivates them for their
+            future desires. He has worked with many NGO’s for the enhancement of
+            the society regarding Women Empowerment and Child Education. He is
+            also the Director of “Shri Kalka Construction Company”.
+          </p>
           <div className="founder__readMoreContainer">
             <button className="founder__readMoreButton">
               <Link to="/founder-1" exact>
@@ -106,14 +108,17 @@ const founders = props => {
         <div className="founder__info">
           <h2>Reena Sharma</h2>
           <h3 className="founder__designation">General Secretary</h3>
-          Reena Sharma is the General Secretary of Saccha Samarpan Society. She
-          is professionally qualified and holds her Masters degree in Master in
-          Social Work (MSW). She has taken an initiative to educate children and
-          support the women equality. She is the Chairperson of Mahila Training
-          Institute (MTI). She has worked with Nishant Nirala Educational and
-          welfare Society as a social worker to promote education in rural
-          areas. She has 20 year of experience in Social Work and Teaching
-          different Vocational courses in different NGOs and Institute.
+          <p>
+            Reena Sharma is the General Secretary of Saccha Samarpan Society.
+            She is professionally qualified and holds her Masters degree in
+            Master in Social Work (MSW). She has taken an initiative to educate
+            children and support the women equality. She is the Chairperson of
+            Mahila Training Institute (MTI). She has worked with Nishant Nirala
+            Educational and welfare Society as a social worker to promote
+            education in rural areas. She has 20 year of experience in Social
+            Work and Teaching different Vocational courses in different NGOs and
+            Institute.
+          </p>
           <div className="founder__readMoreContainer">
             <button className="founder__readMoreButton">
               <Link to="/founder-2" exact>
