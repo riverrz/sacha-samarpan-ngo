6e103dd2b3ead5c9ad45ab6b6c74b0c9af7cb0a9
shivam123425 <shivam123425@gmail.com>
2018-09-25 23:44:26 +0530

    Improve description of members, complete Sponser

--- client/src/components/Sponsers/Sponser/Sponser.js
+++ client/src/components/Sponsers/Sponser/Sponser.js
@@ -1,39 +1,139 @@
 import React, { Component } from "react";
+import { Zoom } from "react-reveal";
 import WhiteContainer from "../../../containers/WhiteContainer/WhiteContainer";
 import "./Sponser.css";
 
 class Sponser extends Component {
   state = {
     sponsers: [
-      { logo: "1.png", description: "", address: "" },
-      { logo: "2.jpg", description: "", address: "" },
-      { logo: "3.png", description: "", address: "" },
-      { logo: "4.jpg", description: "", address: "" },
-      { logo: "5.jpg", description: "", address: "" }
+      {
+        logo: "1.png",
+        title:
+          "Shri Kalka Construction Company ,formerly known as Global Engineers and Contractors ,was established by Mr.Nauratna Gupta in the year 2006.",
+        since: "2006",
+        description:
+          "Shri Kalka Construction Company has lead and worked as an integral part of many ventures in developing integrated townhouses, group housing, shopping malls, multiplexes, hotels and IT parks.This company is the main pillar of Saccha Samarpan which provide and allocate all the beneficial resources to the organisation.",
+        address: "C-53 Jawahar Park, Devli Road,Khanpur, New Delhi-110080",
+        name: "Shri Kalka Construction Company",
+        phone: "9212580053",
+        email: "nauratnag@gmail.com",
+        mapSrc:
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1753.031520288385!2d77.229329975858!3d28.507750995595284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce1a2c2ae96d5%3A0x980a1b570c0cedeb!2sSHRI+KALKA+CONSTRUCTION+CO.!5e0!3m2!1sen!2sin!4v1537898591501"
+      },
+      {
+        logo: "2.jpg",
+        title:
+          "At MTI , its main aim is to create infinite possibilities for our students through high quality education, guidance and academic excellence.",
+        since: "2000",
+        description:
+          "MTI provides various courses for woman like Cutting and Tailoring, Beauty Culture, Computer Course, Fashion Designing and many more.Its main motto is to educate and empower women. Along this, it plays a vital role in Saccha Samarpan and taken an initiative to help and support the organisation to its best.",
+        address: "C-14/2 Jawahar Park, Devli Road, Khanpur, New Delhi-110080",
+        name: "Mahila Training Institute",
+        phone: "9212180053",
+        email: "mti2000@rediffmail.com",
+        mapSrc:
+          "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.119140935912!2d77.22824095055613!3d28.506062982381827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce1a33d8ed9b5%3A0xbd36fa4d47458ebd!2sMahila+Training+Institute!5e0!3m2!1sen!2sin!4v1531673041841"
+      },
+      {
+        logo: "3.png",
+        title:
+          "Ayurvedic Treatment for Hair loss, Weakness, Weight loss/Gain, Sexual Problems and Infertility.",
+        since: "",
+        description:
+          "Dr. Anoop Kumar Gupta provides all the medical support to Saccha Samarpan NGO. He is also our medical advisor.His medical knowledge is very valuable to our NGO.",
+        address: "ED 47, New Ashok Nagar, New Delhi-110096",
+        name: "Sanjeevan Clinic",
+        phone: "9818984848",
+        email: "Anoop12006@gmail.com"
+      },
+      {
+        logo: "4.jpg",
+        title:
+          "All types of N.C.E.R.T Books, stationary and sports items are available. Wholesaler and Retailer",
+        since: "1984",
+        description:
+          "Goyal Book Depot plays an important role in Saccha Samarpan NGO. They support and provide many resources to the organization to accomplish their mission.",
+        address: "A-17/2 Jawahar park Devli Road Khanpur , New Delhi-110080",
+        name: "Goyal Book Depot And Sports",
+        phone: "9718007007",
+        email: "Abhayngfs2000@gmail.com"
+      },
+      {
+        logo: "5.jpg",
+        title:
+          "All types of under garment , lowers ,T-Shirts, gowns, plazo . One of the best Retail Shop for garments.",
+        since: "2010",
+        description:
+          "Aggarwal Undergarments is a major partner and plays a productive role to enhance the growth of the society by helping and supporting Saccha Samarpan NGO.",
+        address: "A-16 Jawahar park Devli Road , Khanpur New Delhi-110080",
+        name: "Aggarwal Undergarments",
+        phone: "9911111475",
+        email: "Aggarwalgarments73@gmail.com"
+      }
     ]
   };
   render() {
     const queryString = this.props.location.search;
     const params = new URLSearchParams(queryString);
-    let selectedSponser = 0;
+    let selectedSponserIndex = 0;
     if (params.has("number")) {
       const passedSponser = Number(params.get("number"));
       if (passedSponser < 5 && passedSponser > -1) {
-        selectedSponser = passedSponser;
+        selectedSponserIndex = passedSponser;
       }
     }
+    let selectedSponser = this.state.sponsers[selectedSponserIndex];
     return (
-      <WhiteContainer style={{ height: "60vh" }}>
+      <WhiteContainer style={{ backgroundColor: "#fff" }}>
         <div className="sponser__container">
-          <div className="sponser__logo__container">
-            <img
-              src={
-                "/images/sponsers/" + this.state.sponsers[selectedSponser].logo
-              }
-              alt="Sponser"
-              className="sponser__logo"
-            />
+          <Zoom>
+            <div className="sponser__logo__container">
+              <img
+                src={"/images/sponsers/" + selectedSponser.logo}
+                alt="Sponser"
+                className="sponser__logo"
+              />
+            </div>
+          </Zoom>
+          <div className="sponser__details__container">
+            <h1 className="sponser__heading--primary">
+              {selectedSponser.name}
+            </h1>
+            <h2 className="sponser__heading--secondary">
+              Since {selectedSponser.since}
+            </h2>
+              <h3 className="sponser__heading--tertiary">
+                {selectedSponser.title}
+              </h3>
+              <p className="sponser__content">{selectedSponser.description}</p>
+              <h4 className="sponser__heading--quaternary">Address</h4>
+              <p className="sponser__content">{selectedSponser.address}</p>
+              <h4 className="sponser__heading--quaternary">Phone No:</h4>
+              <a
+                href={`tel:+91${selectedSponser.phone}`}
+                className="sponser__links"
+              >
+                {selectedSponser.phone}
+              </a>
+              <h4 className="sponser__heading--quaternary">Email Address:</h4>
+              <a
+                href={`mailto:${selectedSponser.email}`}
+                className="sponser__links"
+              >
+                {selectedSponser.email}
+              </a>
           </div>
+          <Zoom>
+            <div className="sponser__map__container">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.119140935912!2d77.22824095055613!3d28.506062982381827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce1a33d8ed9b5%3A0xbd36fa4d47458ebd!2sMahila+Training+Institute!5e0!3m2!1sen!2sin!4v1531673041841"
+                frameBorder="0"
+                allowFullScreen
+                title="sp-map"
+                className="sponser__map"
+              />
+            </div>
+          </Zoom>
         </div>
       </WhiteContainer>
     );
--- client/src/components/Team/Team.js
+++ client/src/components/Team/Team.js
@@ -11,7 +11,7 @@ class Team extends Component {
         photo: "/images/Team/Rajat.jpeg",
         facebook: "https://www.facebook.com/justcoolrajatsharma",
         description:
-          "Rajat Sharma is a well qualified degree holder in B.Tech Computer Science from Jamia Hamdard University. He works as a Computer Teacher in Saccha Samarpan Ngo for the bettlement of child Education. He has taken an initiative of improving child education and giving them the best knowledge and ideas for their bright future."
+          "Rajat Sharma is a well qualified degree holder in B.Tech Computer Science from Jamia Hamdard University. For the betterment of child Education,he is constantly working as a Computer Teacher in Saccha Samarpan Ngo He has taken an initiative of improving child education and giving them the best knowledge and ideas for their bright future in digital India."
       },
       {
         name: "Shivam Kumar",
@@ -19,7 +19,7 @@ class Team extends Component {
         photo: "/images/Team/Shivam.jpg",
         facebook: "https://www.facebook.com/shivam.kumar.79",
         description:
-          "Shivam Kumar is a qualified degree holder in B.Tech Information Technology from Maharaja Agrasen Institute Of Technology with a good experience in the field of Web Technologies. He is the developer of this site and responsible for maintaining the site to allow visitors to get all the proper knowledge about our events, programs and internships."
+          "Shivam Kumar is a qualified degree holder in B.Tech Information Technology from Maharaja Agrasen Institute Of Technology with a good experience in the field of Web Technologies. He is the developer of this website and responsible for maintaining the website to allow visitors to get all the proper insight about our events, programs and internships."
       },
       {
         name: "Rohit Shukla",
@@ -27,7 +27,7 @@ class Team extends Component {
         photo: "/images/Team/Rohit.jpeg",
         facebook: "https://www.facebook.com/rohit.shukla.792",
         description:
-          "I am Rohit Shukla. I have completed my MA in History from Delhi University with first Division. I am also RBI certified from Indian Institute Of Banking and Finance. I have also qualified many competitive exams like CDS,CGL,CISF,ITBP, Delhi Police and Banking, and presently running an competitive exam Institute and hope to teach free of cost in Saccha Samarpan. My vision is that no child is left behind in field of education because of financial deprivation."
+          "Rohit Shukla is one of the dedicated volunteer in Saccha Samarpan NGO. He has completed his MA in History from Delhi University with first Division.He is also RBI certified from Indian Institute Of Banking and Finance. He has also qualified many competitive exams like CDS,CGL,CISF,ITBP, Delhi Police and Banking, and presently running an competitive exam Institute and hope to teach free of cost in Saccha Samarpan. Rohits’s vision is that no child  should be left behind in field of education because of financial deprivation."
       },
       {
         name: "Aman Gupta",
@@ -35,7 +35,7 @@ class Team extends Component {
         photo: "/images/Team/Aman.jpeg",
         facebook: "https://www.facebook.com/AmanGupta0307",
         description:
-          "Aman Gupta devotes all his hardwork,Diligence to this organisation  and  believe in educating child all over the world.He has worked with many NGOs and done a brilliant job everywhere. "
+          "Aman Gupta is a devoted, hard working and diligent volunteer in  this organisation and he believes in educating children all over the world.He has worked with many NGOs and has done a brilliant job everywhere."
       },
       {
         name: "Kaushal Arora",
@@ -43,7 +43,7 @@ class Team extends Component {
         photo: "/images/Team/Kaushal.jpeg",
         facebook: "https://www.facebook.com/kaushal.arora.796",
         description:
-          "Before you give up on yourself remember what your parents gave to just get you where you are. Kaushal Arora is a qualified degree holder in B.Tech computer Science from Jamia Hamdard University. He devotes all his kindness and hardwork  to the organisation."
+          "Kaushal Arora is a qualified degree holder in B.Tech computer Science from Jamia Hamdard University. He devotes all his kindness and hardwork to the organisation. Before you give up on yourself remember what your parents gave to just get you where you are."
       },
       {
         name: "Umesh Rajput",
@@ -51,7 +51,7 @@ class Team extends Component {
         photo: "/images/Team/Umesh.jpeg",
         facebook: "https://www.facebook.com/UmeshRajput43",
         description:
-          "He is one of the most hardworking person of Saccha Samarpan Ngo. He uplifts many issues regarding child education and women empowerment. His main objective is to develop human potential to its fullest extent so that intellectually capable and imaginatively gifted leaders can emerge in a range of professions."
+          "Umesh Rajput  is one of the most hardworking person of Saccha Samarpan Ngo. He uplifts many issues regarding child education and women empowerment. His main objective is to develop human potential to its full extent so that intellectually capable and imaginatively gifted leaders can emerge themselves as professionals."
       },
       {
         name: "Shivanki",
