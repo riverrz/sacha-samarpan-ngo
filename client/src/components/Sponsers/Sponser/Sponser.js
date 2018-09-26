3b0d4be6b26bd641434d406edee664e6bf8b7de0
shivam123425 <shivam123425@gmail.com>
2018-09-26 16:45:44 +0530

    Improve descriptions in Sponser,Team and complete script

--- client/src/components/Sponsers/Sponser/Sponser.js
+++ client/src/components/Sponsers/Sponser/Sponser.js
@@ -9,24 +9,25 @@ class Sponser extends Component {
       {
         logo: "1.png",
         title:
-          "Shri Kalka Construction Company ,formerly known as Global Engineers and Contractors ,was established by Mr.Nauratna Gupta in the year 2006.",
+          "Shri Kalka Construction Company, formerly known as Global Engineers and Contractors,was established by Mr.Nauratna Gupta in the year 2006.",
         since: "2006",
         description:
-          "Shri Kalka Construction Company has lead and worked as an integral part of many ventures in developing integrated townhouses, group housing, shopping malls, multiplexes, hotels and IT parks.This company is the main pillar of Saccha Samarpan which provide and allocate all the beneficial resources to the organisation.",
-        address: "C-53 Jawahar Park, Devli Road,Khanpur, New Delhi-110080",
+          "Shri Kalka Construction Company has lead and worked as an integral part of many ventures in developing integrated townhouses, group housing, shopping malls, multiplexes, hotels and IT parks. This company is the main pillar of Saccha Samarpan NGO which provides and allocates all the beneficial resources to the Organisation.",
+        address:
+          "C-53, G. F. Jawahar Park, Devli Road, Khanpur, New Delhi-110080",
         name: "Shri Kalka Construction Company",
         phone: "9212580053",
-        email: "nauratnag@gmail.com",
+        email: "skcc2006@yahoo.com",
         mapSrc:
           "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1753.031520288385!2d77.229329975858!3d28.507750995595284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce1a2c2ae96d5%3A0x980a1b570c0cedeb!2sSHRI+KALKA+CONSTRUCTION+CO.!5e0!3m2!1sen!2sin!4v1537898591501"
       },
       {
         logo: "2.jpg",
         title:
-          "At MTI , its main aim is to create infinite possibilities for our students through high quality education, guidance and academic excellence.",
+          "At MTI, its main aim is to create infinite possibilities for our students through high quality education, guidance and academic excellence",
         since: "2000",
         description:
-          "MTI provides various courses for woman like Cutting and Tailoring, Beauty Culture, Computer Course, Fashion Designing and many more.Its main motto is to educate and empower women. Along this, it plays a vital role in Saccha Samarpan and taken an initiative to help and support the organisation to its best.",
+          "MTI provides various courses for woman like Cutting and Tailoring, Dress Designing, Fashion Designing, Beauty Culture, Computer Course and many more. Its main motto is to educate and empower women.Along this, it plays a vital role in Saccha Samarpan NGO and taken an initiative to help and support the Organisation to its best.",
         address: "C-14/2 Jawahar Park, Devli Road, Khanpur, New Delhi-110080",
         name: "Mahila Training Institute",
         phone: "9212180053",
@@ -37,10 +38,10 @@ class Sponser extends Component {
       {
         logo: "3.png",
         title:
-          "Ayurvedic Treatment for Hair loss, Weakness, Weight loss/Gain, Sexual Problems and Infertility.",
+          "Ayurvedic Treatment for Hair loss, Weakness in any mode, Weight loss/Gain, Sexual Problems & Female or male Infertility.",
         since: "",
         description:
-          "Dr. Anoop Kumar Gupta provides all the medical support to Saccha Samarpan NGO. He is also our medical advisor.His medical knowledge is very valuable to our NGO.",
+          "Dr. Anoop Kumar Gupta provides all the medical support to Saccha Samarpan NGO. He is also our medical advisor. His medical knowledge is very valuable to our NGO.",
         address: "ED 47, New Ashok Nagar, New Delhi-110096",
         name: "Sanjeevan Clinic",
         phone: "9818984848",
@@ -49,7 +50,7 @@ class Sponser extends Component {
       {
         logo: "4.jpg",
         title:
-          "All types of N.C.E.R.T Books, stationary and sports items are available. Wholesaler and Retailer",
+          "All types of N.C.E.R.T Books, stationary and sports items are available.Wholesaler and Retailer",
         since: "1984",
         description:
           "Goyal Book Depot plays an important role in Saccha Samarpan NGO. They support and provide many resources to the organization to accomplish their mission.",
@@ -61,10 +62,10 @@ class Sponser extends Component {
       {
         logo: "5.jpg",
         title:
-          "All types of under garment , lowers ,T-Shirts, gowns, plazo . One of the best Retail Shop for garments.",
+          "All types of Undergarment, Lowers, T-Shirts, Gowns, Plazo. One of the best Retail Shop for garments.",
         since: "2010",
         description:
-          "Aggarwal Undergarments is a major partner and plays a productive role to enhance the growth of the society by helping and supporting Saccha Samarpan NGO.",
+          "Aggarwal Undergarment is a major partner and plays a productive role to enhance the growth of the society by helping and supporting Saccha Samarpan NGO.",
         address: "A-16 Jawahar park Devli Road , Khanpur New Delhi-110080",
         name: "Aggarwal Undergarments",
         phone: "9911111475",
@@ -102,26 +103,26 @@ class Sponser extends Component {
             <h2 className="sponser__heading--secondary">
               Since {selectedSponser.since}
             </h2>
-              <h3 className="sponser__heading--tertiary">
-                {selectedSponser.title}
-              </h3>
-              <p className="sponser__content">{selectedSponser.description}</p>
-              <h4 className="sponser__heading--quaternary">Address</h4>
-              <p className="sponser__content">{selectedSponser.address}</p>
-              <h4 className="sponser__heading--quaternary">Phone No:</h4>
-              <a
-                href={`tel:+91${selectedSponser.phone}`}
-                className="sponser__links"
-              >
-                {selectedSponser.phone}
-              </a>
-              <h4 className="sponser__heading--quaternary">Email Address:</h4>
-              <a
-                href={`mailto:${selectedSponser.email}`}
-                className="sponser__links"
-              >
-                {selectedSponser.email}
-              </a>
+            <h3 className="sponser__heading--tertiary">
+              {selectedSponser.title}
+            </h3>
+            <p className="sponser__content">{selectedSponser.description}</p>
+            <h4 className="sponser__heading--quaternary">Address</h4>
+            <p className="sponser__content">{selectedSponser.address}</p>
+            <h4 className="sponser__heading--quaternary">Phone No:</h4>
+            <a
+              href={`tel:+91${selectedSponser.phone}`}
+              className="sponser__links"
+            >
+              {selectedSponser.phone}
+            </a>
+            <h4 className="sponser__heading--quaternary">Email Address:</h4>
+            <a
+              href={`mailto:${selectedSponser.email}`}
+              className="sponser__links"
+            >
+              {selectedSponser.email}
+            </a>
           </div>
           <Zoom>
             <div className="sponser__map__container">
