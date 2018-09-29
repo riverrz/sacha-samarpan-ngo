d45ab179f94a532cfcdc7a04b68319d6ca80cad0
shivam123425 <shivam123425@gmail.com>
2018-09-29 13:58:03 +0530

    Improve minor styles and add multiple phone no.

--- client/src/components/Sponsers/Sponser/Sponser.js
+++ client/src/components/Sponsers/Sponser/Sponser.js
@@ -16,7 +16,7 @@ class Sponser extends Component {
         address:
           "C-53, G. F. Jawahar Park, Devli Road, Khanpur, New Delhi-110080",
         name: "Shri Kalka Construction Company",
-        phone: "9212580053",
+        phone1: "9212580053",
         email: "skcc2006@yahoo.com",
         mapSrc:
           "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1753.031520288385!2d77.229329975858!3d28.507750995595284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce1a2c2ae96d5%3A0x980a1b570c0cedeb!2sSHRI+KALKA+CONSTRUCTION+CO.!5e0!3m2!1sen!2sin!4v1537898591501"
@@ -30,7 +30,7 @@ class Sponser extends Component {
           "MTI provides various courses for woman like Cutting and Tailoring, Dress Designing, Fashion Designing, Beauty Culture, Computer Course and many more. Its main motto is to educate and empower women.Along this, it plays a vital role in Saccha Samarpan NGO and taken an initiative to help and support the Organisation to its best.",
         address: "C-14/2 Jawahar Park, Devli Road, Khanpur, New Delhi-110080",
         name: "Mahila Training Institute",
-        phone: "9212180053",
+        phone1: "9212180053",
         email: "mti2000@rediffmail.com",
         mapSrc:
           "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3506.119140935912!2d77.22824095055613!3d28.506062982381827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce1a33d8ed9b5%3A0xbd36fa4d47458ebd!2sMahila+Training+Institute!5e0!3m2!1sen!2sin!4v1531673041841"
@@ -39,12 +39,12 @@ class Sponser extends Component {
         logo: "3.png",
         title:
           "Ayurvedic Treatment for Hair loss, Weakness in any mode, Weight loss/Gain, Sexual Problems & Female or male Infertility.",
-        since: "",
+        since: "2003",
         description:
           "Dr. Anoop Kumar Gupta provides all the medical support to Saccha Samarpan NGO. He is also our medical advisor. His medical knowledge is very valuable to our NGO.",
         address: "ED 47, New Ashok Nagar, New Delhi-110096",
         name: "Sanjeevan Clinic",
-        phone: "9818984848",
+        phone1: "9818984848",
         email: "Anoop12006@gmail.com"
       },
       {
@@ -54,9 +54,10 @@ class Sponser extends Component {
         since: "1984",
         description:
           "Goyal Book Depot plays an important role in Saccha Samarpan NGO. They support and provide many resources to the organization to accomplish their mission.",
-        address: "A-17/2 Jawahar park Devli Road Khanpur , New Delhi-110080",
+        address: "A-17/2, Jawahar Park, Devli Road, Khanpur, New Delhi-110080",
         name: "Goyal Book Depot And Sports",
-        phone: "9718007007",
+        phone1: "9718007007",
+        phone2: "9810660830",
         email: "Abhayngfs2000@gmail.com"
       },
       {
@@ -66,9 +67,10 @@ class Sponser extends Component {
         since: "2010",
         description:
           "Aggarwal Undergarment is a major partner and plays a productive role to enhance the growth of the society by helping and supporting Saccha Samarpan NGO.",
-        address: "A-16 Jawahar park Devli Road , Khanpur New Delhi-110080",
+        address: "A-16, Jawahar Park, Devli Road, Khanpur, New Delhi-110080",
         name: "Aggarwal Undergarments",
-        phone: "9911111475",
+        phone1: "9911111475",
+        phone2: "9911111475",
         email: "Aggarwalgarments73@gmail.com"
       }
     ]
@@ -116,10 +118,16 @@ class Sponser extends Component {
             <div className="sponser__contact__container">
               <h4 className="sponser__heading--quaternary">Phone No:</h4>
               <a
-                href={`tel:+91${selectedSponser.phone}`}
+                href={`tel:+91${selectedSponser.phone1}`}
                 className="sponser__links"
               >
-                {selectedSponser.phone}
+                {selectedSponser.phone1 + " "}
+              </a>
+              <a
+                href={`tel:+91${selectedSponser.phone2}`}
+                className="sponser__links"
+              >
+                {selectedSponser.phone2 ? " , " + selectedSponser.phone2 : null}
               </a>
             </div>
             <div className="sponser__contact__container">
