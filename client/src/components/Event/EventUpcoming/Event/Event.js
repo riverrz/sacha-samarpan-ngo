27ccfe6555ef5cb834e77f970c71e9480353c464
shivam123425 <shivam123425@gmail.com>
2018-10-09 00:18:24 +0530

    Fetch events from /events/fetch/upcoming

--- client/src/components/Event/EventUpcoming/Event/Event.js
+++ client/src/components/Event/EventUpcoming/Event/Event.js
@@ -7,28 +7,19 @@ const event = props => (
     <ul className="event__list">
       <li className="event__list__item">
         <span className="event__list__title">Date:</span>
-        <span className="event__list__detail"> 7th July 2018</span>
+        <span className="event__list__detail"> {props.date}</span>
       </li>
       <li className="event__list__item">
         <span className="event__list__title">Timings:</span>
-        <span className="event__list__detail"> 9:00AM To 12:30PM</span>
+        <span className="event__list__detail"> {props.timings}</span>
       </li>
       <li className="event__list__item">
         <span className="event__list__title">Venue: </span>
-        <span className="event__list__detail">
-          {" "}
-          Bal Bharti Public School, Sector 1, IMT Manesar, Gurugram, Haryana
-          122051
-        </span>
+        <span className="event__list__detail">{props.venue}</span>
       </li>
       <li className="event__list__item">
         <span className="event__list__title">Subject: </span>
-        <span className="event__list__detail">
-          {" "}
-          Orientation on Project CACA, Awareness Legal – Laws related to
-          children safety especially POCSO & Sensitisation Dealing with Child
-          Sexual Abuse and Trauma
-        </span>
+        <span className="event__list__detail">{props.description}</span>
       </li>
     </ul>
   </Fade>
--- client/src/components/Event/EventUpcoming/EventUpcoming.js
+++ client/src/components/Event/EventUpcoming/EventUpcoming.js
@@ -2,53 +2,54 @@ import React, { Component } from "react";
 import WhiteContainer from "../../../containers/WhiteContainer/WhiteContainer";
 import Event from "./Event/Event";
 import "./EventUpcoming.css";
+import MDSpinner from "react-md-spinner";
+import axios from "axios";
+import Error from "../../Error/Error";
 
 class EventUpcoming extends Component {
   state = {
-    events: [
-      {
-        Date: "7th July 2018",
-        Timings: "9:00 AM To 12:30 AM",
-        Venue:
-          "Bal Bharti Public School, Sector 1, IMT Manesar, Gurugram, Haryana 122051",
-        Subject:
-          "Orientation on Project CACA, Awareness Legal – Laws related to children safety especially POCSO & Sensitisation Dealing with Child Sexual Abuse and Trauma"
-      },
-      {
-        Date: "7th July 2018",
-        Timings: "9:00 AM To 12:30 AM",
-        Venue:
-          "Bal Bharti Public School, Sector 1, IMT Manesar, Gurugram, Haryana 122051",
-        Subject:
-          "Orientation on Project CACA, Awareness Legal – Laws related to children safety especially POCSO & Sensitisation Dealing with Child Sexual Abuse and Trauma"
-      },
-      {
-        Date: "7th July 2018",
-        Timings: "9:00 AM To 12:30 AM",
-        Venue:
-          "Bal Bharti Public School, Sector 1, IMT Manesar, Gurugram, Haryana 122051",
-        Subject:
-          "Orientation on Project CACA, Awareness Legal – Laws related to children safety especially POCSO & Sensitisation Dealing with Child Sexual Abuse and Trauma"
-      }
-    ]
+    events: []
   };
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get("/events/fetch/upcoming");
+      this.setState({
+        events: data
+      });
+    } catch (err) {
+      this.setState({
+        error: true
+      });
+    }
+  }
   render() {
-    let events = this.state.events.map((event, id) => {
-      return (
-        <Event
-          date={event.Date}
-          venue={event.Venue}
-          timings={event.Timings}
-          subject={event.Subject}
-          key={id}
-        />
-      );
-    });
+    let content;
+    if (!this.state.events) {
+      content = <MDSpinner size="45" />;
+    } else if (this.state.events.length === 0) {
+      content = <h2 style={{ textAlign: "center" }}>No event found</h2>;
+    } else if (this.state.events.length !== 0) {
+      content = this.state.events.map((event, id) => {
+        console.log(event);
+        return (
+          <Event
+            date={new Date(event.date).toDateString()}
+            venue={event.venue}
+            timings={event.timings}
+            description={event.description}
+            key={id}
+          />
+        );
+      });
+    }
+    if (this.state.error) {
+      content = <Error />;
+    }
 
     return (
-      <WhiteContainer>
+      <WhiteContainer style={{ minHeight: "60vh" }}>
         <h1 className="eventUpcoming__heading--primary">Our Upcoming Events</h1>
-        <div className="eventUpcoming__container">{events}</div>
+        <div className="eventUpcoming__container">{content}</div>
       </WhiteContainer>
     );
   }
