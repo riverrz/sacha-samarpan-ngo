ff912def7d327fd9fbdbef22159bd0cc795cdf7a
Shivam Kumar <shivam123425@gmail.com>
2020-03-29 15:50:53 +0530

    Add advisors

--- client/src/components/Advisors/Advisors.js
+++ client/src/components/Advisors/Advisors.js
@@ -3,59 +3,40 @@ import WhiteContainer from "../../containers/WhiteContainer/WhiteContainer";
 
 import "./Advisors.css";
 
+const data = [
+  {
+    img: "/images/Advisors/advisor-1.jpeg",
+    name: "Shyam Jaju",
+    designation: "National Vice President of BJP",
+    details:
+      "Shyam Jaju (born 29 December 1957) is an Indian politician and the current National Vice President of the Bharatiya Janata Party (BJP) since 2014. He always encouraged our ngo to do great work in the social issues and gives his significant advises to  achieve targets enthusiastically."
+  },
+  {
+    img: "/images/Advisors/advisor-2.jpeg",
+    name: "Sh.Ramchandra Shingare",
+    designation: "SDM(Saket)",
+    details:
+      "He always stands besides us and improve our work with his immense administrative  knowledge and gives us a straight and best path to deal with the sorrows of society."
+  }
+];
+
 const advisors = () => (
   <WhiteContainer>
     <h1 className="advisors__heading--primary">Our Advisors</h1>
     <div className="advisors__container">
       <ul className="advisors__list">
-        <li className="advisors__listItem">
-          <div className="advisors__picContainer">
-            <img
-              src="/images/dummy-advisor.png"
-              alt="Advisor Pic"
-              className="advisors__pic"
-            />
-          </div>
-          <div className="advisors__detailsContainer">
-            <h3 className="advisors__heading--secondary">Dummy</h3>
-            <p className="advisors__designation">
-              IPS Officer, MLA
-            </p>
-            <p className="advisors__details">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
-              quaerat corrupti sapiente dicta magnam accusantium eos excepturi
-              consequuntur molestias voluptatem? Ratione aliquid incidunt
-              molestias nesciunt et aliquam beatae cupiditate mollitia.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
-              quaerat corrupti sapiente dicta magnam accusantium eos excepturi
-              
-            </p>
-          </div>
-        </li>
-        <li className="advisors__listItem">
-          <div className="advisors__picContainer">
-            <img
-              src="/images/dummy-advisor.png"
-              alt="Advisor Pic"
-              className="advisors__pic"
-            />
-          </div>
-          <div className="advisors__detailsContainer">
-            <h3 className="advisors__heading--secondary">Dummy</h3>
-            <p className="advisors__designation">
-              IPS Officer, MLA
-            </p>
-            <p className="advisors__details">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
-              quaerat corrupti sapiente dicta magnam accusantium eos excepturi
-              consequuntur molestias voluptatem? Ratione aliquid incidunt
-              molestias nesciunt et aliquam beatae cupiditate mollitia.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse
-              quaerat corrupti sapiente dicta magnam accusantium eos excepturi
-              
-            </p>
-          </div>
-        </li>
+        {data.map(({ name, details, designation, img }) => (
+          <li key={name} className="advisors__listItem">
+            <div className="advisors__picContainer">
+              <img src={img} alt="Advisor Pic" className="advisors__pic" />
+            </div>
+            <div className="advisors__detailsContainer">
+              <h3 className="advisors__heading--secondary">{name}</h3>
+              <p className="advisors__designation">{designation}</p>
+              <p className="advisors__details">{details}</p>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   </WhiteContainer>
