b13caef66ef5ebbd3ac7fe29af3897df2570f69e
Shivam Kumar <shivam123425@gmail.com>
2020-03-29 16:13:39 +0530

    Add bank details for donation instead of donation form

--- client/src/components/Donate/Donate.js
+++ client/src/components/Donate/Donate.js
@@ -1,5 +1,5 @@
 import React from "react";
-import  "./Donate.css";
+import "./Donate.css";
 import DonateLogo from "../../assets/Icons/donation-1.png";
 import Inputfield from "../Inputfield/Inputfield";
 import WhiteContainer from "../../containers/WhiteContainer/WhiteContainer";
@@ -7,21 +7,17 @@ import WhiteContainer from "../../containers/WhiteContainer/WhiteContainer";
 const donate = props => {
   return (
     <WhiteContainer>
-      <div className='donate__headingContainer'>
-        <h1 className='donate__heading'>
+      <div className="donate__headingContainer">
+        <h1 className="donate__heading">
           Thank You For{" "}
-          <span className='donate__heading__span'>Your Support</span>
-          <div className='donate__logoBox'>
-            <img
-              src={DonateLogo}
-              alt="Donation-1"
-              className='donate__logo'
-            />
+          <span className="donate__heading__span">Your Support</span>
+          <div className="donate__logoBox">
+            <img src={DonateLogo} alt="Donation-1" className="donate__logo" />
           </div>
-          <hr className='donate__horizontal' />
+          <hr className="donate__horizontal" />
         </h1>
       </div>
-      <form autoComplete="off" className='donate__form'>
+      {/* <form autoComplete="off" className='donate__form'>
         <Inputfield type="text" placeholder="Name:" iconClass="fas fa-user" />
         <Inputfield
           type="email"
@@ -48,10 +44,20 @@ const donate = props => {
         <div className='donate__button__container'>
           <button className='donate__button'>Donate</button>
         </div>
-      </form>
-      <div className='donate__conditionsContainer'>
-        <h1 className='donate__conditionsHeading'>Terms & Conditions</h1>
-        <ul className='donate__conditionsList'>
+      </form> */}
+      <div className="donation__details__container">
+        <h3>Given below are our details</h3>
+        <p>Name:- SACCHA SAMARPAN </p>
+        <p>A/C No.:- 5120101003663</p>
+        <p>IFSC: CNRB0005120 </p>
+        <p>Address:- CANARA BANK, KHANPUR, BRANCH, NEW DELHI </p>
+
+        <p>PAN No.:- AATAS1911L</p>
+        <p>Paytm:- 9211807677 </p>
+      </div>
+      <div className="donate__conditionsContainer">
+        <h1 className="donate__conditionsHeading">Terms & Conditions</h1>
+        <ul className="donate__conditionsList">
           <li>
             Refund Policy:- If customer paid twice for one transaction, the one
             transaction amount will be refunded via same source within 07 to 10
